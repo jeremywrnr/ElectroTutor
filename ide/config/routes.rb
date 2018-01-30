8a8c85a6762deb9837b745dbd76a50d9cde0bcc8
Jeremy Warner <jeremywrnr@gmail.com>
2018-01-30 11:04:04 -0500

    clean out old app, and merge react and rails

--- ide/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :microposts
-  resources :users
-
-  root :to => "welcome#index"
-  get 'welcome/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
