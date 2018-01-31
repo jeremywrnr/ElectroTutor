e2aac8a635823030f497e8d3a3f4b34381733ad0
Jeremy Warner <jeremywrnr@gmail.com>
2018-01-31 14:24:55 -0500

    better seeding

--- interface/docs/db/seeds.rb
+++ interface/docs/db/seeds.rb
@@ -1,6 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
+# ActionCable linkage
+note = Note.create!(text: 'hello!', id: 1)
+
+# Application Data
 user = User.create(
   name: 'Quentin',
   email: 'q@q.com'
@@ -9,6 +13,15 @@ user = User.create(
 tuto = Tutorial.create(
   user_id: user.id,
   title: 'sample',
-  description: 'a test tutorial'
-)
+  description: 'a test tutorial')
 
+#test = Test.create()
+
+steps = []
+3.times |i|
+steps << Step.create(
+  user_id: user.id,
+  tutorial_id: tuto.id,
+  title: 'sample step',
+  description: 'a step in the right direction')
+end
