7d9e580dc37d1ee0e7d381bace84090ee0734861
Jeremy Warner <jeremywrnr@gmail.com>
2018-02-09 20:12:29 -0500

    rip out old jwt method

--- interface/api/app/controllers/application_controller.rb
+++ interface/api/app/controllers/application_controller.rb
@@ -4,11 +4,5 @@ class ApplicationController < ActionController::API
   include Response
 
   before_action :authenticate_user
-
-  private
-
-  def authenticate_request
-    @current_user = AuthenticateApiRequest.call(request.headers).result
-    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
-  end
+  #before_action :authenticate_user, only: [:show, :update, :destroy]
 end
