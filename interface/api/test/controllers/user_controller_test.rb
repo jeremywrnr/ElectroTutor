b457a26a7e983f4d195ced462fb09791d6beb3cf
Jeremy Warner <jeremywrnr@gmail.com>
2018-02-09 19:12:59 -0500

    both auth and unauth requests are working

--- interface/api/test/controllers/user_controller_test.rb
+++ interface/api/test/controllers/user_controller_test.rb
@@ -22,4 +22,14 @@ class UserControllerTest < ActionDispatch::IntegrationTest
     get "/users?user_id=#{user.id}", headers: authenticated_header
     assert_response :success
   end
+
+  test "responds correctly without auth" do
+    get "/users?user_id=#{user.id}"
+    assert_response :unauthorized
+  end
+
+  test "responds correctly without auth on different id" do
+    get "/users?user_id=#{user.id+1}"
+    assert_response :unauthorized
+  end
 end
