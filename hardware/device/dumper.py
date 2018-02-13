a1eb10fe69d8efd1c3c153a96572bb0c81be3cd9
Jeremy Warner <jeremywrnr@gmail.com>
2018-02-13 11:16:20 -0500

    cool!!! working hardware hook into the platform io linker

--- hardware/device/dumper.py
+++ hardware/device/dumper.py
@@ -2,7 +2,7 @@ Import("env")
 
 #
 # Dump build environment (for debug)
-# print env.Dump()
+print env.Dump()
 #
 
 #
@@ -12,7 +12,6 @@ Import("env")
 def before_upload(source, target, env):
     print "before_upload"
     # do some actions
-
     # call Node.JS or other script
     env.Execute("node --version")
 
@@ -26,25 +25,19 @@ print "Current build targets", map(str, BUILD_TARGETS)
 env.AddPreAction("upload", before_upload)
 env.AddPostAction("upload", after_upload)
 
-#
 # Custom actions when building program/firmware
-#
+# env.AddPreAction("buildprog", callback...)
+# env.AddPostAction("buildprog", callback...)
 
-env.AddPreAction("buildprog", callback...)
-env.AddPostAction("buildprog", callback...)
-
-#
 # Custom actions for specific files/objects
-#
-
-env.AddPreAction("$BUILD_DIR/${PROGNAME}.elf", [callback1, callback2,...])
-env.AddPostAction("$BUILD_DIR/${PROGNAME}.hex", callback...)
+# env.AddPreAction("$BUILD_DIR/${PROGNAME}.elf", [callback1, callback2,...])
+# env.AddPostAction("$BUILD_DIR/${PROGNAME}.hex", callback...)
 
 # custom action before building SPIFFS image. For example, compress HTML, etc.
-env.AddPreAction("$BUILD_DIR/spiffs.bin", callback...)
+# env.AddPreAction("$BUILD_DIR/spiffs.bin", callback...)
 
 # custom action for project's main.cpp
-env.AddPostAction("$BUILD_DIR/src/main.cpp.o", callback...)
+# env.AddPostAction("$BUILD_DIR/src/main.cpp.o", callback...)
 
 # Custom HEX from ELF
 env.AddPostAction(
@@ -54,3 +47,4 @@ env.AddPostAction(
         "$BUILD_DIR/${PROGNAME}.elf", "$BUILD_DIR/${PROGNAME}.hex"
     ]), "Building $BUILD_DIR/${PROGNAME}.hex")
 )
+
