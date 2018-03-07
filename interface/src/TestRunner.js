3bb895011945be5840226c502cccafa30c3f69c9
Jeremy Warner <jeremy.warner@autodesk.com>
2018-03-07 17:52:54 -0500

    expanding testing types

--- interface/src/TestRunner.js
+++ interface/src/TestRunner.js
@@ -4,13 +4,51 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Message} from 'semantic-ui-react';
+import {Statistic, Icon, Message, Input} from 'semantic-ui-react';
 
 class DynamicRunner extends React.Component {
   render() {
     return (
       <div className="full">
-        <Message content={'dynamic'} />;
+        <Message content={'dynamic'} />
+        {this.props.test.description}
+        <br />
+        {this.props.test.jsondata}
+        <br />
+        {this.props.test.output}
+      </div>
+    );
+  }
+}
+
+class NumericRunner extends React.Component {
+  render() {
+    return (
+      <div className="full">
+        {this.props.test.description}
+        <br />
+        {this.props.test.jsondata}
+        <br />
+        {this.props.test.output}
+        <br />
+        <Statistic color="blue">
+          <Statistic.Value>14</Statistic.Value>
+          <Statistic.Label>measured</Statistic.Label>
+        </Statistic>
+        <Statistic color="green">
+          <Statistic.Value>14</Statistic.Value>
+          <Statistic.Label>expected</Statistic.Label>
+        </Statistic>
+      </div>
+    );
+  }
+}
+
+class MultipleRunner extends React.Component {
+  render() {
+    return (
+      <div className="full">
+        <Message content={'question'} />
         {this.props.test.description}
         <br />
         {this.props.test.jsondata}
@@ -19,31 +57,39 @@ class DynamicRunner extends React.Component {
   }
 }
 
-class NumericRunner extends React.Component {
-  render() {
-    return <Message content={'numeric'} />;
-  }
-}
-
 class QuestionRunner extends React.Component {
   render() {
-    return <Message content={'question'} />;
+    return (
+      <div className="full">
+        <Input placeholder="Answer here..." />
+        {this.props.test.description}
+        <br />
+        {this.props.test.jsondata}
+      </div>
+    );
   }
 }
 
 class ManualRunner extends React.Component {
   render() {
-    return <Message content={'manual'} />;
+    return (
+      <div className="full">
+        <Icon circular name="info" />
+        {this.props.test.description}
+      </div>
+    );
   }
 }
 
 class TestRunner extends React.Component {
   static propTypes = {
-    type: PropTypes.string.isRequired,
+    test: PropTypes.object.isRequired,
+    data: PropTypes.object.isRequired,
   };
 
   static defaultProps = {
-    type: undefined,
+    test: {},
+    data: {},
   };
 
   render() {
@@ -52,6 +98,8 @@ class TestRunner extends React.Component {
         return <DynamicRunner {...this.props} />;
       case 'numeric':
         return <NumericRunner {...this.props} />;
+      case 'multiple':
+        return <MultipleRunner {...this.props} />;
       case 'question':
         return <QuestionRunner {...this.props} />;
       case 'manual':
