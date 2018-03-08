82e8d2303c0febe3610b9307ed058989e7de151e
Jeremy Warner <jeremy.warner@autodesk.com>
2018-03-07 21:06:47 -0500

    better backup tester

--- hardware/backup/tester.cpp
+++ hardware/backup/tester.cpp
@@ -1,16 +1,22 @@
 /**
- * Simple Analog Probe
- * Read an analog value and write it over serial
- */
+   Simple Analog Probe
+   Read an analog value and write it over serial
+*/
 
 #include "Arduino.h"
 
-int probePin = A5;
+int sensed;
+int sense = A0;
+float voltage;
 
 void setup() {
-    Serial.begin(115200);
+  pinMode(sense, INPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
-    Serial.println(analogRead(probePin));
+  sensed = analogRead(sense);
+  voltage = sensed * (5.0 / 1023.0);
+  Serial.println(voltage);
 }
+
