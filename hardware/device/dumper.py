ce08a8b6e6eb0639adfec2919c115bbc50af9ce1
Jeremy Warner <jeremy.warner@autodesk.com>
2018-03-15 10:23:07 -0400

    cleaning dumper and platform

--- hardware/device/dumper.py
+++ hardware/device/dumper.py
@@ -10,20 +10,22 @@ print env.Dump()
 #
 
 def before_upload(source, target, env):
-    print "before_upload"
+    # print "before_upload"
     # do some actions
     # call Node.JS or other script
-    env.Execute("node --version")
+    # env.Execute("node --version")
+    return
 
 
 def after_upload(source, target, env):
-    print "after_upload"
+    # print "after_upload"
     # do some actions
+    return
 
 print "Current build targets", map(str, BUILD_TARGETS)
 
-env.AddPreAction("upload", before_upload)
-env.AddPostAction("upload", after_upload)
+# env.AddPreAction("upload", before_upload)
+# env.AddPostAction("upload", after_upload)
 
 # Custom actions when building program/firmware
 # env.AddPreAction("buildprog", callback...)
@@ -32,9 +34,6 @@ env.AddPostAction("upload", after_upload)
 # Custom actions for specific files/objects
 # env.AddPreAction("$BUILD_DIR/${PROGNAME}.elf", [callback1, callback2,...])
 # env.AddPostAction("$BUILD_DIR/${PROGNAME}.hex", callback...)
-
-# custom action before building SPIFFS image. For example, compress HTML, etc.
-# env.AddPreAction("$BUILD_DIR/spiffs.bin", callback...)
 
 # custom action for project's main.cpp
 # env.AddPostAction("$BUILD_DIR/src/main.cpp.o", callback...)
